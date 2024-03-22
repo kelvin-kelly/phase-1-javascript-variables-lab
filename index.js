8489142344d8e37f61dcc68a68449febf14615e2
Kelvin Machira <machirakelvin81@gmail.com>
2024-03-22 15:53:07 +0300

    Javascript variable lab complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+ let companyName  = "Scuber" ;
+ //const companyName = "Scuber" ;
+
+ // variable called mostProfitableNeighborhood which equate to chelsea 
+  let mostProfitableNeighborhood = "Chelsea" ;
+
+// using let define companyCeo
+ let companyCeo = "Susan Smith" ;
+
+ //
+
+
